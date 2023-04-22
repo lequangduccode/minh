38b04d1704709b91858a7f8b96843f464fad43fd
lequangduccode <88152285+lequangduccode@users.noreply.github.com>
2023-04-22 22:43:25 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function detail(){
+    window.location.href = "http://127.0.0.1:5500/b.html"
+}
+function dog(){
+    window.location.href = "https://bit.ly/3MuiS9o"
+}
+function home(){
+    window.location.href = "http://127.0.0.1:5500/index.html"
+}
+function rer(){
+    window.location.href = "http://127.0.0.1:5500/signup.html"
+}
+function contact(){
+    window.location.href = "http://127.0.0.1:5500/kek.html"
+}
+function donate(){
+    window.location.href = "http://127.0.0.1:5500/donate.html"
+}
+function signin(){
+    window.location.href = "http://127.0.0.1:5500/signin.html"
+}
